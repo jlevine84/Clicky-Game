ee6bf3b54d501ecd971340d03a923b246ee43b31
Jeff Levine <jlevine_84@yahoo.com>
2019-04-26 17:15:28 -0400

    component and styling updates

--- clicky-game/src/components/gameinfo/GameInfo.js
+++ clicky-game/src/components/gameinfo/GameInfo.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import './gameinfo.css'
 
-function GameInfo() {
+function GameInfo(props) {
   return (
     <div>
-      GameInfo
+      <div className="row no-gutters">
+        <div className="instructions">
+          <div className="box">
+            <p><strong>Click on a character to begin!</strong></p>
+            <p><strong>Each character you click have random point values assigned!</strong></p>
+            <p><strong>Clicking on a character you've already clicked will end the game!</strong></p>
+            <p><strong>Beat your high score!</strong></p>
+          </div>
+        </div>
+      </div>
+      <div className="row no-gutters">
+
+        <div className="scores">
+          <div className="box2">
+            <div className="score">
+              <p><strong>Current Score:</strong></p>
+              <p><strong>{props.score}</strong></p>
+            </div>
+            <span className="score">
+              <p><strong>High Score:</strong></p>
+              <p><strong>{props.highScore}</strong></p>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- clicky-game/src/components/gameview/GameView.js
+++ clicky-game/src/components/gameview/GameView.js
@@ -1,20 +1,66 @@
 import React from 'react';
 import GameInfo from '../gameinfo/GameInfo'
-import MainGame from '../maingame/MainGame'
+import Card from '../card/Card'
+import toons from '../../characters.json'
 import "./gameview.css"
+import "../card/card.css"
 
 class GameView extends React.Component {
+  state = {
+    score: 0,
+    highScore: 0,
+    characters: [toons],
+    shuffledCharacters: [],
+    guessed: []
+  }
+
+  componentWillMount() {
+    this.shuffleCards()
+  }
+
+  selectCard = () => {
+    //on card click get the info from the card that was clicked.
+      // check guessed array if not guessed then push to array as valid
+      // update score
+      // if invalid - game ends - alert
+      // check if high score is > current score then update
+      // shuffle cards and reset game values for new game
+  }
+
+  shuffleCards = () => {
+    let newOrder = this.state.characters
+    for (let i = newOrder.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newOrder[i], newOrder[j]] = [newOrder[j], newOrder[i]];
+    }
+    console.log(newOrder)
+    this.setState({
+      characters: newOrder
+    })
+  };
+
   render() {
     return(
       <div>
         <div className="row no-gutters">
           <div className="maingame">
-            <MainGame/>
+            {toons.map(toons => (
+              <Card
+              onClick={this.shuffleCards}
+              id={toons.id}
+              key={toons.id}
+              name={toons.name}
+              image={toons.image}
+              />
+            ))}
           </div>
         </div>
         <div className="row no-gutters">
           <div className="gameinfo">
-            <GameInfo/>
+            <GameInfo
+            score={this.state.score}
+            highScore={this.state.highScore}
+            />
           </div>
         </div>
       </div>
