aaf0531025e648ec77c60793c16a47b7704cbf10
Jeff Levine <jlevine_84@yahoo.com>
2019-04-26 21:14:06 -0400

    Updates

---
+++ precache-manifest.28140d6024dcf5515d9aca45690ddd58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a82e49693cc6e5693adb88c6096d22",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "c668c4ae858bf4957de5",
+    "url": "/Clicky-Game/static/css/main.774b64f8.chunk.css"
+  },
+  {
+    "revision": "e9cacbf2bc110e26e9e8",
+    "url": "/Clicky-Game/static/js/2.dfb0cf60.chunk.js"
+  },
+  {
+    "revision": "c668c4ae858bf4957de5",
+    "url": "/Clicky-Game/static/js/main.b89a22c9.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "4cfa501bfc7720e6ce775f1f73641f04",
+    "url": "/Clicky-Game/static/media/southpark.4cfa501b.jpg"
+  }
+]);
